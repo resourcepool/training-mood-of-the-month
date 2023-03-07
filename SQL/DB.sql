ccb460548f447517527d4fc77db65878d9505588
ArthurAllioux <arthur.allioux@epfedu.fr>
2023-03-07 14:12:58 +0100

    feat(sql DB): added the .sql file

---
+++ SQL/DB.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "Employee" (
+  "uuid" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "email" varchar,
+  "birthdate" timestamp,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "MOTM_Answer" (
+  "uuid" SERIAL PRIMARY KEY,
+  "message" varchar,
+  "Employee_id" integer,
+  "MOTM_id" integer,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "MOTM" (
+  "uuid" SERIAL PRIMARY KEY,
+  "title" varchar,
+  "message_template" varchar,
+  "page_template" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+ALTER TABLE "MOTM_Answer" ADD FOREIGN KEY ("Employee_id") REFERENCES "Employee" ("uuid");
+
+ALTER TABLE "MOTM_Answer" ADD FOREIGN KEY ("MOTM_id") REFERENCES "MOTM" ("uuid");
