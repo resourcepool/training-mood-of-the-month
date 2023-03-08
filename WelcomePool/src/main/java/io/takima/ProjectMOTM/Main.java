923f18a672a9d47620be550e2e42b544db7e35f8
Ivan <ilozano.jds@gmail.com>
2023-03-08 16:41:48 +0100

    Ajout de commandes dans le main pour nourrir et nettoyer la base au démarrage

--- WelcomePool/src/main/java/io/takima/ProjectMOTM/Main.java
+++ WelcomePool/src/main/java/io/takima/ProjectMOTM/Main.java
@@ -5,7 +5,16 @@ import org.hibernate.cfg.*;
 public class Main {
 
     public static void main(String[] args) {
-        SessionFactory sessionFactory = config.buildSessionFactory();
-        Session session = sessionFactory.openSession();
+//        SessionFactory sessionFactory = config.buildSessionFactory();
+//        Session session = sessionFactory.openSession();
+
+        RecordsOnInitService recordsOnInitService = new RecordsOnInitService();
+        recordsOnInitService.onInit();
+
+        EmployeeService employeeService = new EmployeeService();
+        employeeService.getEmployees();
+
+        MotmService motmService = new MotmService();
+        motmService.getAllMotm();
     }
 }
