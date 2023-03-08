41eb05abb88244d7daf992e1ee442b5e955ca9f4
Ivan <ilozano.jds@gmail.com>
2023-03-08 11:31:14 +0100

    Ajout de EmployeeJDBC pour connecter et faire des statements sql à la base

---
+++ WelcomePool/src/main/java/io/takima/ProjectMOTM/EmployeeJDBC.java
@@ -0,0 +1,75 @@
+package io.takima.ProjectMOTM;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeJDBC {
+    // TODO Connect DB : open statement close
+
+    // TODO Statement : insert et select
+
+    // TODO
+
+
+
+
+    String dbURL = "jdbc:postgresql://localhost:5432/welcomepool";
+    String username = "postgres";
+    String password = "postgres";
+
+    public EmployeeJDBC() {
+    }
+
+    public EmployeeJDBC(String dbURL, String username, String password) {
+        this.dbURL = dbURL;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void connect() {
+        try {
+            Connection conn = DriverManager.getConnection(dbURL, username, password);
+
+            if (conn != null) {
+                System.out.println("Connected");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    public List<Employee> getAllEmployees(String sqlQuery) {
+
+        List<Employee> employeeList = new ArrayList<>();
+
+        try {
+            Connection conn = DriverManager.getConnection(dbURL, username, password);
+
+            if (conn != null) {
+                System.out.println("Connected");
+
+                Statement statement = conn.createStatement();
+                ResultSet result = statement.executeQuery(sqlQuery);
+
+
+                while (result.next()){
+                    Employee employee = new Employee(result.getString("name"), result.getString("email"), result.getDate("birthdate"));
+                    employeeList.add(employee);
+
+                    System.out.println(employee.getName());
+                }
+
+                conn.close();
+                System.out.println("Connection closed");
+
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return employeeList;
+    }
+}
