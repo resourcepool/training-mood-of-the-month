c69a116fadccebaa8b24c163461a5dd3fd5d62b7
Ivan <ilozano.jds@gmail.com>
2023-03-08 16:43:00 +0100

    Suppression méthode connect inutile

--- WelcomePool/src/main/java/io/takima/ProjectMOTM/EmployeeJDBC.java
+++ WelcomePool/src/main/java/io/takima/ProjectMOTM/EmployeeJDBC.java
@@ -27,17 +27,6 @@ public class EmployeeJDBC {
         this.password = password;
     }
 
-    public void connect() {
-        try {
-            Connection conn = DriverManager.getConnection(dbURL, username, password);
-
-            if (conn != null) {
-                System.out.println("Connected");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
 
 
     public List<Employee> getAllEmployees(String sqlQuery) {
