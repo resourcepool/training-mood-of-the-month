5b4519f8ef18ca542fbdacb0d50a008c384b7ba6
Ivan <ilozano.jds@gmail.com>
2023-03-08 16:43:36 +0100

    Ajout de commentaires

--- WelcomePool/src/main/java/io/takima/ProjectMOTM/RecordsOnInitService.java
+++ WelcomePool/src/main/java/io/takima/ProjectMOTM/RecordsOnInitService.java
@@ -4,8 +4,12 @@ public class RecordsOnInitService {
 
     public void onInit(){
         RecordsJDBC recordsJDBC = new RecordsJDBC();
+
+        // Vider la table Employee et la table Motm
         recordsJDBC.flushEmployeeOnInit();
         recordsJDBC.flushMotmOnInit();
+
+        // Insérer des données au démarrage pour pouvoir bosser dessus en dev
         recordsJDBC.insertRecordsInEmployeeOnInit();
     }
 }
