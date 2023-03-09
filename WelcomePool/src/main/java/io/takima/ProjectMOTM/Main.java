dc68dc2559cda698e643ebc718937397ba30f0fe
Ivan <ilozano.jds@gmail.com>
2023-03-09 17:55:22 +0100

    Suppression du corps du main car sans utilité pour le moment

--- WelcomePool/src/main/java/io/takima/ProjectMOTM/Main.java
+++ WelcomePool/src/main/java/io/takima/ProjectMOTM/Main.java
@@ -4,13 +4,6 @@ package io.takima.ProjectMOTM;
 public class Main {
 
     public static void main(String[] args) {
-        RecordsOnInitService recordsOnInitService = new RecordsOnInitService();
-        recordsOnInitService.onInit();
 
-        EmployeeService employeeService = new EmployeeService();
-        employeeService.getEmployees();
-
-        MotmService motmService = new MotmService();
-        motmService.getAllMotm();
     }
 }
